1c47f870dc59ff97c85b8bfb5cec03505f841640
Jose Rebelo <jose.lucas@vtex.com>
2021-10-10 17:17:16 -0300

    adding base files and project

---
+++ MusiCon/Controllers/AlbumController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusiCon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusiCon.Controllers
+{
+    [Route("api/album")]
+    [ApiController]
+    public class AlbumController : ControllerBase
+    {
+        Album albumglobal; //declarei uma variavel do tipo Album em scope global
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Album>> Get()
+        {
+            return new Album[] { albumglobal }; //criei o array de album, listando elemento teste
+        }
+
+        [HttpPost]
+        public void CreateAlbum([FromBody] Album value)
+        {
+            albumglobal = value; //salvo o valor recebido no parametro no escopo global (poderia ser um banco)
+        }
+    }
+}
---
+++ MusiCon/Models/Album.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusiCon.Models
+{
+    public class Album
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }       
+    }
+}
